eea293a7cff3f02b32873a32ec388f178256cccb
fredrfha <fredrfha@stud.ntnu.no>
2022-10-16 09:32:13 +0200

    Prettier

--- frontend/src/components/LoadingIndicator/LoadingIndicator.tsx
+++ frontend/src/components/LoadingIndicator/LoadingIndicator.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './LoadingIndicator.css';
 
 const LoadingIndicator = () => {
-    return <div className="container">
-                <div className="loadingBall1"></div>
-                <div className="loadingBall2"></div>
-                <div className="loadingBall3"></div>
-            </div>;
+  return (
+    <div className="container">
+      <div className="loadingBall1"></div>
+      <div className="loadingBall2"></div>
+      <div className="loadingBall3"></div>
+    </div>
+  );
 };
 
 export default LoadingIndicator;
--- frontend/src/components/Searchbar/Searchbar.tsx
+++ frontend/src/components/Searchbar/Searchbar.tsx
@@ -6,89 +6,112 @@ import SearchIcon from '@mui/icons-material/Search';
 import LoadingIndicator from '../LoadingIndicator/LoadingIndicator';
 
 interface ApiResponse {
-    article: string,
-    status: number,
-    data?: any,
-    occurrences?: number
+  article: string;
+  status: number;
+  data?: any;
+  occurrences?: number;
 }
 
 function Searchbar() {
+  const [loading, setLoading] = useState(false);
+  const [searchString, setSearchString] = useState('');
+  const [response, setResponse] = useState<ApiResponse>(); // json object
 
-    const [loading, setLoading] = useState(false);
-    const [searchString, setSearchString] = useState('');
-    const [response, setResponse] = useState<ApiResponse>(); // json object
+  const updateSearchString = (event: any) => {
+    setSearchString(event.target.value);
+  };
 
-    const updateSearchString = (event: any) => {
-        setSearchString(event.target.value);
-    };
+  // fetches from api every time searchString changes
+  useEffect(() => {
+    if (searchString == null || searchString === '') {
+      return;
+    }
+    setLoading(true);
+    fetch(`http://localhost:8080/article/${searchString}`, {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setResponse(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, [searchString]);
 
-    // fetches from api every time searchString changes
-    useEffect(() => {
-        if (searchString == null || searchString === '') {
-            return;
-        }
-        setLoading(true);
-        fetch(`http://localhost:8080/article/${searchString}`, {
-            method: 'GET',
-        })
-        .then((res) => res.json())
-        .then(res => {
-            setResponse(res);
-            setLoading(false);
-        })
-        .catch((err) => {
-            console.error(err);
-            setLoading(false);
-        })
-    }, [searchString]);
-
-    // Displays the word count of the article
-    const textToReturn = () => {
-        if (searchString === '' || loading) {
-            return;
-        }
-        
-        if (response?.status !== 200) {
-            return <p className="occurrenceText">😐 Fant ingen artikkel</p>;
-        } else {
-            return <p className="occurrenceText"><b>{response?.occurrences}</b> ord</p>;
-        }
-    };
-
-    // adds red border to textField if no article is found
-    const textFieldToReturn = () => {
-        if (response?.status !== 200 && searchString !== '') {
-            return <TextField error fullWidth label="Søk wikipedia" className="searchTextField" variant="outlined" onChange={updateSearchString} value={searchString} InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <SearchIcon />
-                  </InputAdornment>
-                )
-              }} />;
-        } else {
-            return <TextField fullWidth color="secondary" label="Søk wikipedia" className="searchTextField" variant="outlined" onChange={updateSearchString} value={searchString} InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <SearchIcon />
-                  </InputAdornment>
-                )
-              }} />;
-        }
-    };
-
-    const displayLoading = () => {
-        return loading ? <LoadingIndicator /> : null;
+  // Displays the word count of the article
+  const textToReturn = () => {
+    if (searchString === '' || loading) {
+      return;
     }
 
-    return (
-        <>
-            {textFieldToReturn()}
-            <div className="textToReturnBox">
-                {textToReturn()}
-            </div>
-            {displayLoading()}
-        </>
-    );
+    if (response?.status !== 200) {
+      return <p className="occurrenceText">😐 Fant ingen artikkel</p>;
+    } else {
+      return (
+        <p className="occurrenceText">
+          <b>{response?.occurrences}</b> ord
+        </p>
+      );
+    }
+  };
+
+  // adds red border to textField if no article is found
+  const textFieldToReturn = () => {
+    if (response?.status !== 200 && searchString !== '') {
+      return (
+        <TextField
+          error
+          fullWidth
+          label="Søk wikipedia"
+          className="searchTextField"
+          variant="outlined"
+          onChange={updateSearchString}
+          value={searchString}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      );
+    } else {
+      return (
+        <TextField
+          fullWidth
+          color="secondary"
+          label="Søk wikipedia"
+          className="searchTextField"
+          variant="outlined"
+          onChange={updateSearchString}
+          value={searchString}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      );
+    }
+  };
+
+  const displayLoading = () => {
+    return loading ? <LoadingIndicator /> : null;
+  };
+
+  return (
+    <>
+      {textFieldToReturn()}
+      <div className="textToReturnBox">{textToReturn()}</div>
+      {displayLoading()}
+    </>
+  );
 }
 
 export default Searchbar;
